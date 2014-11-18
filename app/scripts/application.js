187b2a76f10a818a50ce9c94866fc938cfb6526f
Yasuaki KAWAKAMI <bougan1160@gmail.com>
2014-11-18 18:06:24 +0900

    ・DialogにNativeの機能を追加 ・DialogでNativeの機能が使えない場合はHTMLで生成するように変更 ・画面回転時のDialogの再描画方法を変更  Fixes #3

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,86 +1,100 @@
 define([
-    'backbone',
-    'communicator',
-    'routers/router',
-    'regions/content',
-    'views/view/nav',
-    'views/view/header',
-    'views/view/dialog',
-    'modules/data_manager',
-    'zepto.touch',
-    'uajudge'
-],
+		'backbone',
+		'communicator',
+		'routers/router',
+		'regions/content',
+		'views/view/nav',
+		'views/view/header',
+		'views/view/dialog',
+		'modules/data_manager',
+		'zepto.touch',
+		'uajudge'
+	],
 
-    function (Backbone, Communicator, Router, ContentRegion, NavView, HeaderView, DialogView, DataManager) {
-        'use strict';
+	function (Backbone, Communicator, Router, ContentRegion, NavView, HeaderView, DialogView, DataManager) {
+		'use strict';
 
-        var App = new Backbone.Marionette.Application();
-        App.module("DataManager", DataManager);
+		var App = new Backbone.Marionette.Application();
+		App.module("DataManager", DataManager);
 
-        /* Add application regions here */
-        App.addRegions({
-            'header': '#header',
-            'content': {
-                selector: "#content",
-                regionType: ContentRegion
-            },
-            'dialog': '#dialog',
-            'nav': '#nav',
-            'toast': '#toast'
-        });
+		/* Add application regions here */
+		App.addRegions({
+			'header': '#header',
+			'content': {
+				selector: "#content",
+				regionType: ContentRegion
+			},
+			'dialog': '#dialog',
+			'nav': '#nav',
+			'toast': '#toast'
+		});
 
-        /* Add initializers here */
-        App.addInitializer(function () {
-            /*  window.console.log = function () {
-             };*/
-            //Phonegapのプラグイン使用準備
-            document.addEventListener('deviceready', function () {
-                console.log('Device is Ready');
-                //AndroidのBack Buttonの制御
-                document.addEventListener("backbutton", function () {
-                    Communicator.command.execute('back');
-                }, false);
-            }, false);
+		/* Add initializers here */
+		App.addInitializer(function () {
+			/*  window.console.log = function () {
+			 };*/
+			//Phonegapのプラグイン使用準備
+			document.addEventListener('deviceready', function () {
+				console.log('Device is Ready');
+				//AndroidのBack Buttonの制御
+				document.addEventListener("backbutton", function () {
+					Communicator.command.execute('back');
+				}, false);
+			}, false);
 
-            //レイアウトの初期化
-            //App.nav.attachView(new NavView({el: App.nav.el}));
-            App.header.attachView(new HeaderView({el: App.header.el}));
-            App.dialog.attachView(new DialogView({el: App.dialog.el}).render());
-            //App.toast.attachView(new ToastView({el: App.toast.el}));
+			//レイアウトの初期化
+			//App.nav.attachView(new NavView({el: App.nav.el}));
+			App.header.attachView(new HeaderView({el: App.header.el}));
+			App.dialog.attachView(new DialogView({el: App.dialog.el}).render());
+			//App.toast.attachView(new ToastView({el: App.toast.el}));
 
-            //端末に応じて画面回転のリスナーを設定
-            var $win = $(window);
-            if (ua.isiOS) {
-                $win.bind('resize', function () {
-                    Communicator.mediator.trigger('resize');
-                });
-            }
-            if (ua.isAndroid) {
-                $win.bind('resize', function () {
-                    Communicator.mediator.trigger('resize');
-                });
-            }
-            if (ua.isiPad || ua.isTablet) {
-                console.log("isTablet");
-                $('html').css('font-size', '75%');
-                Communicator.mediator.trigger('resize');
-            }
+			//端末に応じて画面回転のリスナーを設定
+			var $win = $(window);
+			if (ua.isiOS) {
+				//resizeイベントは複数発火してしまう
+				//browserでは発火しない
+				$win.bind('resize', function () {
+					console.log("resize");
+					Communicator.mediator.trigger('resize');
+				});
+			}
+			if (ua.isAndroid) {
+				$win.bind('resize', function () {
+					Communicator.mediator.trigger('resize');
+				});
+			}
+			if (ua.isiPad || ua.isTablet) {
+				console.log("isTablet");
+				$('html').css('font-size', '75%');
+				Communicator.mediator.trigger('resize');
+			}
 
-            Communicator.command.execute("showDialog", {
-                title: "Build is Success",
-                message: "Hello World!! ",
-                ok: function () {
-                }
-            });
+			_.delay(function () {
+				Communicator.command.execute("showDialog", 'prompt', {
+					title: "Build is Success",
+					message: "Hello World!! ",
+					buttonNames: ["o-k-", "cancel-"],
+					data: ["test1", "test2", "test3", "test4","test1", "test2", "test3", "test4"],
+					ok: function (text) {
+						console.log("OK is Worked " + text);
+					},
+					cancel: function () {
+						console.log("Cancel is worked");
+					},
+					defaultIndex: 3,
+					isSecure: false,
+					defaultText: "test"
+				});
+			}, 1000);
 
-            this.listenToOnce(App.DataManager, 'DB:ready', function () {
-                //ルーター準備
-                new Router();
-                Backbone.history.start();
-                Communicator.command.execute('changePage', 'firstPage');
-            });
-            App.DataManager.start();
-        });
+			this.listenToOnce(App.DataManager, 'DB:ready', function () {
+				//ルーター準備
+				new Router();
+				Backbone.history.start();
+				Communicator.command.execute('changePage', 'firstPage');
+			});
+			App.DataManager.start();
+		});
 
-        return App;
-    });
+		return App;
+	});
--- app/scripts/views/view/dialog.js
+++ app/scripts/views/view/dialog.js
@@ -1,180 +1,362 @@
 define([
-    'backbone',
-    'communicator',
-    'hbs!tmpl/view/dialog_tmpl',
-    'hbs!tmpl/view/dialog_list_tmpl'
-],
-    function (Backbone, Communicator, DialogTmpl, DialogListTmpl) {
-        'use strict';
+		'backbone',
+		'communicator',
+		'hbs!tmpl/view/dialog_tmpl',
+		'hbs!tmpl/view/dialog_picker_tmpl',
+		'hbs!tmpl/view/dialog_alert_tmpl',
+		'hbs!tmpl/view/dialog_prompt_tmpl',
+		'hbs!tmpl/view/dialog_progress_tmpl'
+	],
+	function (Backbone, Communicator, DialogTmpl, DialogPickerTmpl, DialogAlertTmpl, DialogPromptTmpl, DialogProgressTmpl) {
+		'use strict';
 
-        /* Return a Layout class definition */
-        return Backbone.Marionette.ItemView.extend({
+		/* Return a Layout class definition */
+		return Backbone.Marionette.ItemView.extend({
 
-            tagName: "div",
+			tagName: "div",
 
-            initialize: function () {
-                console.log("initialize a Dialog Layout");
-                Communicator.command.setHandler('showDialog', this.showDialog, this);
-                Communicator.command.setHandler('hideDialog', this.hideDialog, this);
-                this.listenTo(Communicator.mediator, 'resize', this.onRender);
-            },
+			initialize: function () {
+				console.log("initialize a Dialog Layout");
+				Communicator.command.setHandler('showDialog', this.showDialog, this);
+				Communicator.command.setHandler('hideDialog', this.hideDialog, this);
+				this.listenTo(Communicator.mediator, 'resize', this.dialogRender);
+			},
 
-            template: DialogTmpl,
+			template: DialogTmpl,
 
-            /* ui selector cache */
-            ui: {
-                'panel': '.dialog-panel',
-                'title': '#dialog-title',
-                'content': '#dialog-content',
-                'buttons': '.dialog-buttons',
-                'ok': '#dialog-ok-button',
-                'cancel': '#dialog-cancel-button'
-            },
+			/* ui selector cache */
+			ui: {
+				'panel': '.dialog-panel',
+				'title': '.dialog-title',
+				'content': '.dialog-content',
+				'buttons': '.dialog-buttons',
+				'ok': '#dialog-ok-button',
+				'cancel': '#dialog-cancel-button'
+			},
 
-            /* Ui events hash */
-            events: {
-                'tap #dialog-ok-button': 'ok',
-                'tap #dialog-cancel-button': 'cancel'
-            },
+			/* Ui events hash */
+			events: {
+				'tap @ui.ok': 'ok',
+				'tap @ui.cancel': 'cancel'
+			},
 
-            showDialog: function (options) {
-                this.ui.title.hide();
-                this.ui.content.attr('class', 'dialog-content').hide();
-                this.ui.ok.hide();
-                this.ui.cancel.hide();
-                this.ui.buttons.hide();
-                this.ui.panel.addClass('dialog-ready');
+			/**
+			 * ダイアログの制御
+			 *
+			 * @param {string} dialogType - ダイアログの種類
+			 * @param {object} options - ダイアログのオプション
+			 */
+			showDialog: function (dialogType, options) {
+				if (this.isDialogShown) {
+					this.hideDialog();
+				}
+				this.isDialogShown = true;
 
-                if (options.hasOwnProperty('template')) {
-                    console.log("Prepare View of " + options.template);
-                    this.getTemplateView(options.template, options);
-                } else {
-                    this.ui.title.html(options.title);
-                    this.ui.content.html(options.message);
+				var ok = (options.ok || function () {
+				});
+				var cancel = (options.cancel || function () {
+				});
 
-                    if (options.hasOwnProperty('ok')) {
-                        this.okFunc = options.ok;
-                        this.ui.ok.show();
-                        this.ui.buttons.show();
-                    } else {
-                        this.okFunc = function () {
-                        };
-                    }
+				switch (dialogType) {
+					case "alert":
+						this.showAlert(options.title, options.message, ok, options.buttonName);
+						break;
+					case "confirm":
+						this.showConfirm(options.title, options.message, ok, cancel, options.buttonNames);
+						break;
+					case "prompt":
+						this.showPrompt(options.title, options.message, ok, cancel, options.buttonNames, options.defaultText, options.isSecure);
+						break;
+					case "progress":
+						this.showProgress(options.title, options.message);
+						break;
+					case "picker":
+						this.showPicker(options.title, options.data, ok, cancel, options.defaultIndex);
+						break;
+				}
+			},
 
-                    if (options.hasOwnProperty('cancel')) {
-                        this.cancelFunc = options.cancel;
-                        this.ui.cancel.show();
-                        this.ui.buttons.show();
-                    } else {
-                        this.cancelFunc = function () {
-                        };
-                    }
-                    this.ui.title.show();
-                    this.ui.content.show();
-                }
-                this.onRender();
+			/**
+			 * Alertの表示
+			 *
+			 * @param {string} title - タイトル
+			 * @param {string} message - メッセージ
+			 * @param {function} ok - callback関数
+			 * @param {string} [buttonName=OK] - ボタンの名前
+			 */
+			showAlert: function (title, message, ok, buttonName) {
+				console.log('showAlert');
+				var _title = (title || "Alert");
+				var _message = (message || "This message is Default.");
+				var _ok = (ok || function () {
+				});
+				var _buttonName = (buttonName || "OK");
+				if (navigator && navigator.notification && navigator.notification.alert) {
+					console.log("Show native Dialog");
+					console.log(_message, _title, _buttonName);
+					navigator.notification.alert(_message, _ok, _title, _buttonName);
+				} else {
+					console.log("No native dialogs");
+					var $content = $(Backbone.Marionette.Renderer.render(DialogAlertTmpl, {message: _message}));
+					this.showHTMLDialog(_title, $content, _ok, null, [_buttonName]);
+				}
+			},
 
-                this.$el.addClass('dialog-show').removeClass('dialog-hide');
-                console.log("Show Dialog");
-            },
+			/**
+			 * Confirmの表示
+			 *
+			 * @param {string} title - タイトル
+			 * @param {string} message - メッセージ
+			 * @param {function} ok - OKが押された時のcallback関数
+			 * @param {function} cancel - Cancelが押された時のcallback関数
+			 * @param {array} [buttonNames=¥[OK,Cancel¥]] - ボタンの名前
+			 */
+			showConfirm: function (title, message, ok, cancel, buttonNames) {
+				var _title = (title || "Alert");
+				var _message = (message || "This message is Default.");
+				var _callback = function (buttonIndex) {
+					if (buttonIndex == 1) {
+						ok();
+					} else {
+						cancel();
+					}
+				};
+				var _buttonNames = (buttonNames || ["OK", "Cancel"]);
+				if (navigator && navigator.notification && navigator.notification.confirm) {
+					navigator.notification.confirm(_message, _callback, _title, _buttonNames);
+				} else {
+					console.log("No native");
+					var $content = $(Backbone.Marionette.Renderer.render(DialogAlertTmpl, {message: _message}));
+					this.showHTMLDialog(_title, $content, ok, cancel, _buttonNames);
+				}
+			},
 
-            hideDialog: function () {
-                console.log("Hide Dialog");
-                this.$el.addClass('dialog-hide').removeClass('dialog-show');
-            },
+			/**
+			 * Prompt(入力フィールド付きDialog)を表示
+			 *
+			 * @param {string} title - タイトル
+			 * @param {string} message - メッセージ
+			 * @param {function} ok - OKが押された時のcallback関数(引数に入力された値)
+			 * @param {function} cancel - Cancelが押された時のcallback関数
+			 * @param {array} [buttonNames=¥[OK,Cancel¥]] - ボタンの名前
+			 * @param {string} [defaultText] - デフォルトのテキスト
+			 * @param {boolean} [isSecure] - SecureInputの有無
+			 */
+			showPrompt: function (title, message, ok, cancel, buttonNames, defaultText, isSecure) {
+				var _title = (title || "Alert");
+				var _message = (message || "This message is Default.");
+				var _callback = function (results) {
+					if (results.buttonIndex == 1) {
+						ok(results.input1);
+					} else {
+						cancel();
+					}
+				};
+				var _buttonNames = (buttonNames || ["OK", "Cancel"]);
+				var _defaultText = (defaultText || "");
+				var _promptType = (isSecure) ? "secure" : "normal";
+				if (navigator && navigator.notification && navigator.notification.prompt) {
+					navigator.notification.prompt(_message, _callback, _title, _buttonNames, _defaultText, _promptType);
+				} else {
+					var $content = $(Backbone.Marionette.Renderer.render(DialogPromptTmpl, {
+						message: _message,
+						defaultText: _defaultText,
+						isSecure: isSecure
+					}));
+					var _ok = function () {
+						ok($content.siblings('input').val());
+					};
+					this.showHTMLDialog(_title, $content, _ok, cancel, _buttonNames);
+				}
+			},
 
-            ok: function () {
-                this.okFunc();
-                this.hideDialog();
-            },
+			/**
+			 * Progressの表示
+			 * @param {string} title - タイトル
+			 * @param {string} message - メッセージ
+			 * @param {string} [type=simple] - (未実装)ProgressDialogのタイプ(simple | bar)
+			 * @param {string} [value] - (未実装)進捗状況(1-100)
+			 */
+			showProgress: function (title, message, type, value) {
+				console.log("Show Progress");
+				var _title = (title || "Loading...");
+				var _message = (message || "");
+				var isDialogCreated = false;
+				var $content = "";
 
-            cancel: function () {
-                this.cancelFunc();
-                this.hideDialog();
-            },
+				if ((window.device && window.device.platform == "iOS") || ua.isiOS) {
+					console.log("iOS");
+					if (window.ProgressIndicator) {
+						console.log("Native");
+						if (_.isEmpty(_message)) {
+							window.ProgressIndicator.showSimpleWithLabel(false, _title);
+							isDialogCreated = true;
+						}
+						else {
+							window.ProgressIndicator.showSimpleWithLabelDetail(false, _title, _message);
+							isDialogCreated = true;
+						}
+					}
 
-            getTemplateView: function (templateName, options) {
-                switch (templateName) {
-                    case 'update':
-                        var img = $('<i class="fa fa-refresh fa-2x fa-spin"></i>').css({"vertical-align": "middle", "margin-right": "1rem"});
-                        this.ui.content.html(img.get(0).outerHTML + '旧バージョンから更新中...').css('text-align', 'left');
-                        this.ui.content.show();
-                        break;
-                    case 'accessServer':
-                        var img = $('<i class="fa fa-refresh fa-2x fa-spin"></i>').css({"vertical-align": "middle", "margin-right": "1rem"});
-                        this.ui.content.html(img.get(0).outerHTML + '情報取得中...').css('text-align', 'left');
-                        this.ui.content.show();
-                        break;
-                    case 'timeout':
-                        this.ui.title.text('エラー');
-                        this.ui.content.html("サーバーとの通信に失敗しました。<BR>お手数ですがネットワーク環境をご確認の上再度お試しください。");
-                        this.okFunc = function () {
-                        };
+					if (!isDialogCreated) {
+						$content = $(Backbone.Marionette.Renderer.render(DialogProgressTmpl, {
+							title: _title,
+							message: _message
+						}));
+					}
+				} else if ((window.device && window.device.platform == "Android") || ua.isAndroid) {
+					if (navigator && navigator.notification && navigator.notification.activityStart) {
+						navigator.notification.activityStart(_title, _message);
+						isDialogCreated = true;
+					}
+					if (!isDialogCreated) {
+						$content = $(Backbone.Marionette.Renderer.render(DialogProgressTmpl, {
+							title: "",
+							message: _message
+						}));
+					}
+				}
 
-                        this.ui.title.show();
-                        this.ui.content.show();
-                        this.ui.ok.show();
-                        this.ui.buttons.show();
-                        break;
-                    case 'list':
-                        this.ui.title.text(options.title);
-                        this.ui.content.html(this.getListView(options.options));
-                        var hide = _.bind(this.hideDialog, this);
-                        this.ui.content.find('.dialog-list-item').bind('tap', function (e) {
-                            options.callback($(e.currentTarget).text(), $(e.currentTarget).attr("data-action"));
-                            hide();
-                        });
-                        this.cancelFunc = function () {
-                        };
+				if (!isDialogCreated) {
+					this.showHTMLDialog(_title, $content);
+				}
+			},
 
-                        this.ui.title.show();
-                        this.ui.content.addClass('dialog-list').show();
-                        this.ui.buttons.show();
-                        this.ui.cancel.show();
-                        break;
-                    case 'input':
-                        this.ui.title.text(options.title);
-                        var input = $('<input type="text" class="dialog-input">');
-                        input.val(options.value);
-                        this.ui.content.html(input);
-                        this.okFunc = function () {
-                            options.ok(input.val());
-                        };
-                        this.cancelFunc = function () {
-                        };
+			/**
+			 * Pickerの表示
+			 *
+			 * @param {string} title - [Androidのみ]タイトル
+			 * @param {array} data - リストデータ
+			 * @param {function} ok - OKが押された時のcallback関数(引数に入力された値)
+			 * @param {function} cancel - Cancelが押された時のcallback関数
+			 * @param {number} defaultIndex - [iOSのみ]Defaultの選択肢
+			 */
+			showPicker: function (title, data, ok, cancel, defaultIndex) {
+				var _title = (title || "Please Pick");
+				var _data = (data || ["List1", "List2"]);
+				var _callback = function (result) {
+					if (result.buttonIndex == 1) {
+						ok(result.selectedIndex);
+					} else {
+						cancel();
+					}
+				};
+				var _defaultIndex = (defaultIndex || 0);
+				var isDialogCreated = false;
 
-                        this.ui.title.show();
-                        this.ui.content.show();
-                        this.ui.buttons.show();
-                        this.ui.ok.show();
-                        this.ui.cancel.show();
-                        break;
-                }
-            },
+				if ((window.device && window.device.platform == "iOS") || ua.isiOS) {
+					console.log("show native");
+					if (window.PickerView && window.PickerView.show) {
+						window.PickerView.show(_data, _defaultIndex, _callback);
+						isDialogCreated = true;
+					}
+				}
+				else if ((window.device && window.device.platform == "Android") || ua.isAndroid) {
+					if (navigator && navigator.notification && navigator.notification.list) {
+						navigator.notification.list(_title, _data, _callback);
+						isDialogCreated = true;
+					}
+				}
 
-            getListView: function (data) {
-                var values;
-                if (_.isArray(data)) {
-                    values = data;
-                } else {
-                    values = _.keys(data);
-                }
-                return Backbone.Marionette.Renderer.render(DialogListTmpl, {values: values});
-            },
+				if (!isDialogCreated) {
+					var options = _.map(_data, function (option, index) {
+						return {val: option, selected: index == _defaultIndex}
+					});
+					var $content = $(Backbone.Marionette.Renderer.render(DialogPickerTmpl, {
+						options: options
+					}));
+					var hide = _.bind(this.hideDialog, this);
+					$content.siblings('div').on('tap', function (e) {
+						hide();
+						ok($(e.currentTarget).attr('data-index'));
+					});
+					this.showHTMLDialog(_title, $content, cancel, null, ["Cancel"]);
+				}
+			},
 
-            /* on render callback */
-            onRender: function () {
-                this.ui.panel.css('max-height', this.$el.height() * 0.8);
-                _.defer(function (panel) {
-                    panel.css({
-                        'margin-top': '-' + (panel.height() / 2) + 'px',
-                        'margin-left': '-' + (panel.width() / 2) + 'px'
-                    });
-                    _.defer(function () {
-                        panel.removeClass('dialog-ready');
-                    });
-                }, this.ui.panel);
-            }
-        });
+			/**
+			 * HTML版のDialogを表示
+			 *
+			 * @param {string} title - タイトル
+			 * @param {object} $content - 表示データ
+			 * @param {function} ok - OKが押された時のcallback関数(引数に入力された値)
+			 * @param {function} cancel - Cancelが押された時のcallback関数
+			 * @param {array} buttonNames - ボタンの名前
+			 */
+			showHTMLDialog: function (title, $content, ok, cancel, buttonNames) {
+				this.ui.title.text(title);
+				this.ui.content.empty().append($content);
+				if (_.isUndefined(buttonNames) || buttonNames == 0) {
+					this.ui.buttons.hide();
+					this.ui.ok.hide();
+					this.ui.cancel.hide();
+				} else if (buttonNames.length == 1) {
+					this.ui.buttons.show();
+					this.ui.ok.text(buttonNames[0]);
+					this.ui.ok.show();
+					this.ui.cancel.hide();
+					this.okCallback = ok;
+				} else if (buttonNames.length == 2) {
+					this.ui.buttons.show();
+					this.ui.ok.text(buttonNames[0]);
+					this.ui.ok.show();
+					this.okCallback = ok;
+					this.ui.cancel.text(buttonNames[1]);
+					this.ui.cancel.show();
+					this.cancelCallback = cancel;
+				}
 
-    });
+				this.dialogRender();
+				this.$el.show();
+			},
+
+			hideDialog: function () {
+				console.log("Hide Dialog");
+				this.isDialogShown = false;
+				this.$el.hide();
+				this.ui.panel.addClass("dialog-prepare");
+				if (ua.isiOS && window.ProgressIndicator)
+					window.ProgressIndicator.hide();
+				else if (ua.isAndroid && window.navigator && window.navigator.notification && window.navigator.notification.activityStop)
+					window.navigator.notification.activityStop();
+			},
+
+			ok: function () {
+				this.hideDialog();
+				if (_.isFunction(this.okCallback)) {
+					this.okCallback();
+				}
+			},
+
+			cancel: function () {
+				this.hideDialog();
+				if (_.isFunction(this.cancelCallback)) {
+					this.cancelCallback();
+				}
+			},
+
+			dialogRender: function () {
+				console.log("Dialog Render");
+				if (!this.ui.panel.hasClass("dialog-prepare")) {
+					this.ui.panel.addClass("dialog-prepare");
+				}
+				this.ui.panel.css('max-height', this.$el.height() * 0.8);
+				_.defer(function (panel) {
+					panel.css({
+						'margin-top': '-' + (panel.height() / 2) + 'px',
+						'margin-left': '-' + (panel.width() / 2) + 'px'
+					});
+
+					//CSSの適用に時間がかかるため表示を遅らせる
+					_.delay(function () {
+						panel.removeClass('dialog-prepare');
+					}, 50);
+				}, this.ui.panel);
+			},
+
+			/* on render callback */
+			onRender: function () {
+			}
+		});
+
+	});
